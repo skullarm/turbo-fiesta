d46e00244df20a3c6e3e5dc0dac4598c13dbf820
skullarm <skullarm8387@gmail.com>
2025-12-23 17:24:43 +0000

    Sanitize non-admin requested URLs: case-insensitive keyword substitutions in path/query/hash (Gwilliam->williams, Andrew->andy, A.J/AJ->jim)

--- worker.js
+++ worker.js
@@ -127,6 +127,53 @@ export default {
           return;
         }
 
+        // If requester is not admin, apply safe, case-insensitive keyword substitutions
+        // Only modify pathname/search/hash to avoid changing hostnames
+        if (!admin) {
+          const replacements = [
+            { r: /\bGwilliam\b/gi, v: 'williams' },
+            { r: /\bAndrew\b/gi, v: 'andy' },
+            { r: /\bA\.?J\.?\b/gi, v: 'jim' }
+          ];
+          try {
+            const uo = new URL(normalizedU);
+            const originalPart = uo.pathname + uo.search + uo.hash;
+            let modifiedPart = originalPart;
+            for (const rep of replacements) modifiedPart = modifiedPart.replace(rep.r, rep.v);
+            if (modifiedPart !== originalPart) {
+              // split modifiedPart into pathname, search and hash
+              const hashIndex = modifiedPart.indexOf('#');
+              const searchIndex = modifiedPart.indexOf('?');
+              let pathname = modifiedPart;
+              let search = '';
+              let hash = '';
+              if (searchIndex !== -1 && (hashIndex === -1 || searchIndex < hashIndex)) {
+                pathname = modifiedPart.slice(0, searchIndex);
+                if (hashIndex !== -1) {
+                  search = modifiedPart.slice(searchIndex, hashIndex);
+                  hash = modifiedPart.slice(hashIndex);
+                } else {
+                  search = modifiedPart.slice(searchIndex);
+                }
+              } else if (hashIndex !== -1) {
+                pathname = modifiedPart.slice(0, hashIndex);
+                hash = modifiedPart.slice(hashIndex);
+              }
+              uo.pathname = pathname || '/';
+              uo.search = search;
+              uo.hash = hash;
+              normalizedU = uo.toString();
+            }
+          } catch (e) {
+            // Fallback: simple string replacements if URL parsing fails
+            let newU = normalizedU;
+            for (const rep of [{ r: /\bGwilliam\b/gi, v: 'williams' }, { r: /\bAndrew\b/gi, v: 'andy' }, { r: /\bA\.?J\.?\b/gi, v: 'jim' }]) {
+              newU = newU.replace(rep.r, rep.v);
+            }
+            normalizedU = newU;
+          }
+        }
+
         // Build cache key
         let cacheUrl;
         try {
